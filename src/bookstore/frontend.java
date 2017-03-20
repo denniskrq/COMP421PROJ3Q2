707fdac54d68246eae096e2ec5f37bf1a2a8bf37
abehnan <abehnan@gmail.com>
2017-03-20 19:13:21 -0400

    Added functionality

--- src/bookstore/frontend.java
+++ src/bookstore/frontend.java
@@ -1,15 +1,13 @@
 import java.util.Scanner;
 import java.sql.*;
 
-    public class frontend {
-    public static void main(String[] args)
-    {
-        while (true)
-        {
+public class frontend {
+    public static void main(String[] args) {
+        while (true) {
             // gui stuff
             int selection = 0;
             System.out.println("\nMENU");
-            System.out.println("Option 1: approve a book");
+            System.out.println("Option 1: manage book approval");
             System.out.println("Option 2: XXX");
             System.out.println("Option 3: XXX");
             System.out.println("Option 4: XXX");
@@ -19,11 +17,11 @@ import java.sql.*;
             Scanner sc = new Scanner(System.in);
             selection = sc.nextInt();
             System.out.printf("You selected option %d\n", selection);
-            
+
             if (selection == 6)
                 break;
             else if (selection == 1)
-                approveBook();
+                manageBookStatus();
             else if (selection == 2)
                 option2();
             else if (selection == 3)
@@ -31,74 +29,90 @@ import java.sql.*;
             else if (selection == 4)
                 option4();
             else if (selection == 5)
-                option5();  
+                option5();
         }
         System.out.println("Exiting...");
         System.exit(0);
     }
 
-    private static void approveBook()
+    private static void manageBookStatus()
     {
-        Connection c = null;
-        Statement stmt = null;
+        Scanner scan = new Scanner(System.in);
         int selection;
-        try {
-            Class.forName("org.postgresql.Driver");
-            c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/cs421", "cs421g02", "Team2pass");
-            System.out.println("Opened database successfully");
-
-            while(true)
-            {
-                stmt = c.createStatement();
+        
+        while(true)
+        {
+            try {
+                Class.forName("org.postgresql.Driver");
+                Connection c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/cs421", "cs421g02", "Team2pass");
+                System.out.println("Opened database successfully");
+                Statement stmt = c.createStatement();
                 ResultSet rs = stmt.executeQuery("SELECT * FROM BOOK;");
                 while (rs.next())
                 {
                     int bid = rs.getInt("bid");
                     boolean statusFlag = rs.getBoolean("statusflag");
                     String title = rs.getString("title");
-                    System.out.println("\tbid: " + bid + "\t\tstatusFlag: " + statusFlag + "\t\ttitle: " + title);
+                    System.out.println("bid: " + bid + "\tstatusFlag: " + statusFlag + "\ttitle: " + title);
                 }
                 rs.close();
-
-                System.out.print("Please enter the bid of the book you want to approve (enter 0 to quit): ");
-                Scanner sc = new Scanner(System.in);
-                selection = sc.nextInt();
-                if (selection == 0)
-                {
-                    stmt.close();
-                    c.close();
-                    return;
-                }
-                stmt.executeUpdate("UPDATE BOOK SET STATUSFLAG = true WHERE bid=" + selection + ";");
                 stmt.close();
+                c.close();
             }
-        }
-        catch(Exception e)
-        {
-            System.err.println(e.getClass().getName()+": " + e.getMessage());
-            System.exit(0);
-        }
-        System.out.println("Command executed succesffully!");
+            catch(Exception e)
+            {
+                System.err.println(e.getClass().getName()+": " + e.getMessage());
+                System.exit(0);
+            }
+            System.out.println("got here 1");
+            System.out.print("enter bid of book whose status will be changed (enter 0 to quit): ");
+            selection = scan.nextInt();
+            if (selection == 0)
+                return;
+    
+            else
+            {
+                try
+                {
+                    Class.forName("org.postgresql.Driver");
+                    Connection c2 = DriverManager.getConnection("jdbc:postgresql://localhost:5432/cs421", "cs421g02", "Team2pass");
+                    System.out.println("Opened database successfully");
+                    Statement stmt2 = c2.createStatement();
+                    ResultSet rs2 = stmt2.executeQuery("select statusflag from book where bid=" + selection + ";");
+                    while (rs2.next())
+                    {
+                        boolean statusFlag2 = rs2.getBoolean("statusflag");
+                        boolean newStatus = !statusFlag2;
+                        Statement stmt3 = c2.createStatement();
+                        stmt3.executeUpdate("UPDATE BOOK SET STATUSFLAG =" + newStatus + " WHERE bid=" + selection + ";");
+                        stmt3.close();
+                    }
+                    rs2.close();
+                    stmt2.close();
+                    c2.close();
+                }
+                catch(Exception e)
+                {
+                    System.err.println(e.getClass().getName()+": " + e.getMessage());
+                    System.exit(0);
+                }
+            }
+        }   
+    }
+
+    private static void option2() {
 
     }
 
-    private static void option2()
-    {
+    private static void option3() {
 
     }
 
-    private static void option3()
-    {
+    private static void option4() {
 
     }
 
-    private static void option4()
-    {
-
-    }
-
-    private static void option5()
-    {
+    private static void option5() {
 
     }
 }
