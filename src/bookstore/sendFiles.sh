79b6df551d0624e348cd27a5f4b460fea7395a62
abehnan <abehnan@gmai.com>
2017-03-21 00:25:59 -0400

    Improved script

--- src/bookstore/sendFiles.sh
+++ src/bookstore/sendFiles.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 javac -cp ./postgresql-42.0.0.jre7.jar frontend.java &&
-scp frontend.class cs421g02@comp421.cs.mcgill.ca:
+scp frontend.class cs421g02@comp421.cs.mcgill.ca: && 
+ssh -t cs421g02@comp421.cs.mcgill.ca './runFrontend.sh';
